85e2f7b8021ce175db9086a97c56a361991cab6d
Vladimir Grankov <Vladimirgrankov@yandex.ru>
2024-06-18 13:33:46 +0300

    Terminal Application

---
+++ src/shared/types/index.ts
@@ -0,0 +1,7 @@
+export { City } from './city.type.js';
+export { Location } from './location.type.js';
+export { User } from './user.type.js';
+export { UserType } from './user.type.js';
+export { OfferType } from './offer.type.js';
+export { Review } from './review.type.js';
+export { Offer } from './offer.type.js';
